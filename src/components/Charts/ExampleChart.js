20922ae3c7bc491da8d7f394cbf65338ebb13e12
yournewgoodname <damislav@net.hr>
2022-04-25 19:02:43 +0200

    finished charts

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -11,32 +11,46 @@ import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const ChartComponent = ({ data }) => {
-  // STEP 3 - Creating the JSON object to store the chart configurations
-  const chartConfigs = {
-    type: "pie3d", // The chart type
-    width: "400", // Width of the chart
-    height: "400", // Height of the chart
-    dataFormat: "json", // Data type
-    dataSource: {
-      // Chart Configuration
-      chart: {
-        //Set the chart caption
-        caption: "Most popular language",
-        //Set the chart subcaption
-        subCaption: "In MMbbl = One Million barrels",
-        //Set the x-axis name
-        xAxisName: "Country",
-        //Set the y-axis name
-        yAxisName: "Reserves (MMbbl)",
-        numberSuffix: "K",
-        //Set the theme for your chart
-        theme: "fusion",
-      },
-      // Chart Data
-      data,
+const chartData = [
+  {
+    label: "HTML",
+    value: "13",
+  },
+  {
+    label: "CSS",
+    value: "23",
+  },
+  {
+    label: "Javascript",
+    value: "80",
+  },
+];
+// STEP 3 - Creating the JSON object to store the chart configurations
+const chartConfigs = {
+  type: "column2d", // The chart type
+  width: "400", // Width of the chart
+  height: "400", // Height of the chart
+  dataFormat: "json", // Data type
+  dataSource: {
+    // Chart Configuration
+    chart: {
+      //Set the chart caption
+      caption: "Most popular language",
+      //Set the chart subcaption
+      subCaption: "In MMbbl = One Million barrels",
+      //Set the x-axis name
+      xAxisName: "Country",
+      //Set the y-axis name
+      yAxisName: "Reserves (MMbbl)",
+      numberSuffix: "K",
+      //Set the theme for your chart
+      theme: "fusion",
     },
-  };
+    // Chart Data
+    data: chartData,
+  },
+};
+const ChartComponent = ({data,}) => {
   return <ReactFC {...chartConfigs} />;
 };
 
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,34 +1,64 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { GithubContext } from "../context/context";
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 
 const Repos = () => {
   const { repos } = useContext(GithubContext);
+
   let languages = repos.reduce((total, item) => {
-    const { language } = item;
+    const { language, stargazers_count } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
     return total;
   }, {});
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
+  //  most stars per language
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
 
+  // stars , forks
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  console.log(repos);
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
   return (
     <section className="section-center">
       <Wrapper>
-        <Pie3D data={languages} />
-        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
